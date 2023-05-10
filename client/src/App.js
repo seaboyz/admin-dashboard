5a5de9bb2b7236ee4e1b77d401536a56d34e03f8
seaboyz <seaboyz@hotmail.com>
2023-05-10 10:02:04 -0500

    Add theme to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,16 @@
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { themeSettings } from "theme.js";
+
 function App() {
+  const mode = useSelector(state => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode), [mode]));
   return (
     <div className="app">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
     </div>
   );
 }
--- client/src/state/index.js
+++ client/src/state/index.js
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     mode: "dark"
+    // mode: "light"
 };
 
 export const globalSlice = createSlice({
