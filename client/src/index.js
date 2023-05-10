f146ce8bfe30c47e92140986e2d80dca9c457c35
seaboyz <seaboyz@hotmail.com>
2023-05-10 09:43:10 -0500

    Add redux/toolkit stater files

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { configureStore } from "@reduxjs/toolkit";
+import globalReducer from "state";
+import { Provider } from "react-redux";
+
+const store = configureStore({
+  reducer: {
+    global: globalReducer
+  }
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
+
