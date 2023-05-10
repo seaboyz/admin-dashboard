49ade094ab56454a757e5c7ec3f12280d65f8f32
seaboyz <seaboyz@hotmail.com>
2023-05-10 10:27:26 -0500

    Add router to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
-import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { Dashboard } from "@mui/icons-material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { themeSettings } from "theme.js";
 
 function App() {
@@ -8,10 +11,19 @@ function App() {
   const theme = useMemo(() => createTheme(themeSettings(mode), [mode]));
   return (
     <div className="app">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-      </ThemeProvider>
-    </div>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />}>
+              {/* when reach the "/", browser will be redirected to "/dashboard" */}
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div >
   );
 }
 
