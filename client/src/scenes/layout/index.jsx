a96970cced9baa49c1287482a53a5bd25d10e50f
seaboyz <seaboyz@hotmail.com>
2023-05-12 14:00:33 -0500

    Add Sidebar to Layout

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -1,13 +1,30 @@
-import { Box } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
 import Navbar from "components/Navbar.jsx";
-import React from 'react';
+import Sidebar from "components/Sidebar.jsx";
+import React, { useState } from 'react';
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
+    // mobile < 600px
+    const isNoneMobile = useMediaQuery("(min-width:600px)");
+    const [isSidebarOpen, setSidebarOpen] = useState(true);
     return (
-        <Box width="100%" heigh="100%">
-            <Box>
-                <Navbar />
+        <Box
+            display={isNoneMobile ? "flex" : "block"}
+            width="100%"
+            height="100%"
+        >
+            <Sidebar
+                isNoneMobile={isNoneMobile}
+                drawerWidth="250px"
+                isSidebarOpen={isSidebarOpen}
+                setSidebarOpen={setSidebarOpen}
+            />
+            <Box
+                width="100%"
+                height="100%"
+            >
+                <Navbar isSidebarOpen={isSidebarOpen} />
                 <Outlet />
             </Box>
         </Box>
