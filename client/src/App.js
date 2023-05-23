f607271e61e3066bb66f04d72dd1d44111561f74
seaboyz <seaboyz@hotmail.com>
2023-05-23 14:14:55 -0500

    Add dark/light mode icon to toggle mode

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,26 @@
-import { Button, createTheme, CssBaseline, ThemeProvider, Typography } from "@mui/material";
+import { DarkModeOutlined, LightModeOutlined } from "@mui/icons-material";
+import { createTheme, CssBaseline, IconButton, ThemeProvider, Typography } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { setMode } from "redux/slice.js";
 import { themeSettings } from "theme.js";
 
 function App() {
   const mode = useSelector(state => state.global.mode);
-  const darkTheme = createTheme(themeSettings(mode));
+  const theme = createTheme(themeSettings(mode));
   const dispatch = useDispatch();
 
 
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={theme}>
       {/* enable the dark mode for the whole app */}
       <CssBaseline />
-      <Typography variant="h3">
-        App
-      </Typography>
-      <Button color="primary" onClick={() => dispatch(setMode())}>Toggle Mode</Button>
+      <IconButton onClick={() => dispatch(setMode())}>
+        {
+          theme.palette.mode === "dark"
+            ? <DarkModeOutlined sx={{ fontSize: "25px" }} />
+            : <LightModeOutlined sx={{ fontSize: "25px" }} />
+        }
+      </IconButton>
     </ThemeProvider>
   );
 }
