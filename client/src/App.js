82681730d478c8359a4008ac796cb2b4de01a7db
seaboyz <seaboyz@hotmail.com>
2023-05-23 11:55:13 -0500

    Enable MUI default dark mode

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
+import { createTheme, CssBaseline, ThemeProvider, Typography } from "@mui/material";
+
 function App() {
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+    }
+  });
   return (
-    <div className="app">
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Typography variant="h3">
+        App
+      </Typography>
+    </ThemeProvider>
   );
 }
 
